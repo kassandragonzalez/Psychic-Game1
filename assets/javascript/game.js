31201f8be43c58c0c2b1f3ee1e92a8f814e537ff
Kassandra Gonzalez <kgkassieg@gmail.com>
2019-10-10 21:54:07 -0500

    index and js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,39 @@
-// Created an array of the computer choices and user guesses
+/*
 
-//var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-//var userGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var letter = ["a", "b", "c", "d", "e"];
+var compuGuess = letter[0];
 
-var computerChoices = ["a"];
-var userGuesses = ["a"];
+document.onkeyup = function(event) {
+  var userGuess = event.key.toLowerCase();
+  console.log("Keypressed:", event.key);
 
-// Created wins/losses and guesses variables
-
-var Wins = 0;
-var Losses = 0;
-var guessesLeft = 9;
-var userGuesses = "";
-
-// Create variable that refrence the html
-
-var directionsText = document.getElementById("directions-text");
-var userGuesses = document.getElementById("userGuesses-text");
-var winsText = document.getElementById("wins-text");
-var guessesLeft = document.getElementById("Guessesleft-text");
-
-
-// This function is run whenever the user presses a key.
-
-document.onkeyup = function (event) {
-
-    // Determines which key was pressed.
-
-    var userGuess = event.key;
-
-    // Randomly chooses a choice from the options array. This is the Computer's guess.
-
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    
-    // Reworked our code from last step to use "else if" instead of lots of if statements.
-    // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
-   
-    if (userGuess === "a") {
-        if ((userGuess === "r" && computerGuess === "s") ||
-            (userGuess === "s" && computerGuess === "p") ||
-            (userGuess === "p" && computerGuess === "r")) {
-            wins++;
-        } else if (userGuess === computerGuess) {
-            ties++;
-        } else {
-            losses++;
-        }
-       
-        // Hide the directions
-        
-        directionsText.textContent = "";
-        
-        // Display the user and computer guesses, and wins/losses/ties.
-        
-        userChoiceText.textContent = "You chose: " + userGuess;
-        computerChoiceText.textContent = "The computer chose: " + computerGuess;
-        winsText.textContent = "wins: " + wins;
-        lossesText.textContent = "losses: " + losses;
-        tiesText.textContent = "ties: " + ties;
-    }
+  if (compuGuess == userGuess) {
+    alert("You Win!");
+  } else {
+    alert("You Lost!");
+  }
 };
+
+//for Psycic game 
+*/
+
+//For Word Game
+
+/*
+This goes in html:
+
+<div id="user-progress"> --- </div>
+*/
+var words = ["apple", "pizza", "watermelon", "tacos"];
+var compuChoice = words[0];
+console.group(compuChoice);
+var targetWord = compuChoice.split("");
+console.log(targetWord);
+
+function makeDashes() {
+  for (var i = 0; i < targetWord.length; i++) {
+    var userProgress = document.getElementById("user-progress");
+    var theDashes = "";
+    userProgress.textContent += "_";
+  }
+}
