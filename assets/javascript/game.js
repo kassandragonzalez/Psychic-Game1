e2880060be370384e134a3d47253e0f81aef5e82
Kassandra Gonzalez <kgkassieg@gmail.com>
2019-10-14 06:43:26 -0500

    psychic game re-done, issues with git

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,68 @@
-/*
+var letter = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
 
-var letter = ["a", "b", "c", "d", "e"];
-var compuGuess = letter[0];
+var compuGuess = letter[Math.floor(Math.random() * letter.length)];
+var guessCounter = 9;
+var guessedLetter = [];
+
+var wins = 0;
+var losses = 0;
+
+var userGuessText = document.getElementById("userguesses-text");
+var guessCounterText = document.getElementById("num-guesses-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
 
 document.onkeyup = function(event) {
   var userGuess = event.key.toLowerCase();
   console.log("Keypressed:", event.key);
 
-  if (compuGuess == userGuess) {
-    alert("You Win!");
+  
+  if (userGuess == compuGuess) {
+    wins++;
+    guessCounter = 9;
   } else {
-    alert("You Lost!");
+    if (guessCounter != 1) {
+      guessCounter--;
+    } else {
+      losses++;
+      guessCounter = 9;
+    }
   }
+
+  userGuessText.textContent = "You chose: " + userGuess;
+  guessCounterText.textContent = "Guesses left: " + guessCounter;
+  winsText.textContent = "wins: " + wins;
+  lossesText.textContent = "losses: " + losses;
 };
 
-//for Psycic game 
-*/
+//for Psychic game
 
 //For Word Game
 
@@ -24,7 +71,7 @@ This goes in html:
 
 <div id="user-progress"> --- </div>
 */
-var words = ["apple", "pizza", "watermelon", "tacos"];
+/*var words = ["apple", "pizza", "watermelon", "tacos"];
 var compuChoice = words[0];
 console.group(compuChoice);
 var targetWord = compuChoice.split("");
@@ -36,4 +83,4 @@ function makeDashes() {
     var theDashes = "";
     userProgress.textContent += "_";
   }
-}
+}*/
